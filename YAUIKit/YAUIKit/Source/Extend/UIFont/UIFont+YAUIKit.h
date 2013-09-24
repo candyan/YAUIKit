f917551759d71df6dae1fea2ad80252ccee9e9cf
Candyan <liuyanhp@gmail.com>
2013-09-24 15:20:39 +0800

    add UIFont category to create Helvetica font

---
+++ YAUIKit/YAUIKit/Source/Extend/UIFont/UIFont+YAUIKit.h
@@ -0,0 +1,15 @@
+//
+//  UIFont+YAUIKit.h
+//  YAUIKit
+//
+//  Created by liuyan on 13-9-24.
+//  Copyright (c) 2013年 Douban Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (YAUIKit)
+
++ (UIFont *)helveticaFontOfSize:(CGFloat)size;
+
+@end
---
+++ YAUIKit/YAUIKit/Source/Extend/UIFont/UIFont+YAUIKit.m
@@ -0,0 +1,18 @@
+//
+//  UIFont+YAUIKit.m
+//  YAUIKit
+//
+//  Created by liuyan on 13-9-24.
+//  Copyright (c) 2013年 Douban Inc. All rights reserved.
+//
+
+#import "UIFont+YAUIKit.h"
+
+@implementation UIFont (YAUIKit)
+
++ (UIFont *)helveticaFontOfSize:(CGFloat)size
+{
+  return [UIFont fontWithName:@"Helvetica" size:size];
+}
+
+@end
