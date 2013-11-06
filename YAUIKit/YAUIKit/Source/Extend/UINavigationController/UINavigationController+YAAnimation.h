049bf6b93c8eae1aad476529f03c3fdebf9f8cdb
Candyan <liuyanhp@gmail.com>
2013-11-06 14:49:48 +0800

    add push & pop viewController method that providing UIViewController in animation Block

--- YAUIKit/YAUIKit/Source/Extend/UINavigationController/UINavigationController+YAAnimation.h
+++ YAUIKit/YAUIKit/Source/Extend/UINavigationController/UINavigationController+YAAnimation.h
@@ -8,37 +8,88 @@
 
 #import <UIKit/UIKit.h>
 
+typedef void(^YAAnimationLayoutViewsBlock)(UIView *fromView, UIView *toView);
+typedef void(^YAAnimationLayoutViewControllersBlock)(UIViewController *fromViewController, UIViewController *toViewController);
+
 @interface UINavigationController (YAAnimation)
+
+#pragma mark - Push
 
 - (void)pushViewController:(UIViewController *)toViewController
                   duration:(NSTimeInterval)duration
-                prelayouts:(void (^)(UIView *fromView, UIView *toView))preparation
-                animations:(void (^)(UIView *fromView, UIView *toView))animations
-                completion:(void (^)(UIView *fromView, UIView *toView))completion;
+                prelayouts:(YAAnimationLayoutViewsBlock)preparation
+                animations:(YAAnimationLayoutViewsBlock)animations
+                completion:(YAAnimationLayoutViewsBlock)completion;
 
 - (void)pushViewController:(UIViewController *)toViewController
                   duration:(NSTimeInterval)duration
                    options:(UIViewAnimationOptions)options
-                prelayouts:(void (^)(UIView *fromView, UIView *toView))preparation
-                animations:(void (^)(UIView *fromView, UIView *toView))animations
-                completion:(void (^)(UIView *fromView, UIView *toView))completion;
+                prelayouts:(YAAnimationLayoutViewsBlock)preparation
+                animations:(YAAnimationLayoutViewsBlock)animations
+                completion:(YAAnimationLayoutViewsBlock)completion;
+
+- (void)pushViewController:(UIViewController *)toViewController
+                  duration:(NSTimeInterval)duration
+      controllerPrelayouts:(YAAnimationLayoutViewControllersBlock)preparation
+      controllerAnimations:(YAAnimationLayoutViewControllersBlock)animations
+                completion:(YAAnimationLayoutViewControllersBlock)completion;
+
+- (void)pushViewController:(UIViewController *)toViewController
+                  duration:(NSTimeInterval)duration
+                   options:(UIViewAnimationOptions)options
+      controllerPrelayouts:(YAAnimationLayoutViewControllersBlock)preparation
+      controllerAnimations:(YAAnimationLayoutViewControllersBlock)animations
+                completion:(YAAnimationLayoutViewControllersBlock)completion;
+
+
+#pragma mark - Pop
 
 - (UIViewController *)popViewControllerWithDuration:(NSTimeInterval)duration
-                                         prelayouts:(void (^)(UIView *fromView, UIView *toView))preparation
-                                         animations:(void (^)(UIView *fromView, UIView *toView))animations
-                                         completion:(void (^)(UIView *fromView, UIView *toView))completion;
+                                         prelayouts:(YAAnimationLayoutViewsBlock)preparation
+                                         animations:(YAAnimationLayoutViewsBlock)animations
+                                         completion:(YAAnimationLayoutViewsBlock)completion;
 
 - (UIViewController *)popViewControllerWithDuration:(NSTimeInterval)duration
                                             options:(UIViewAnimationOptions)options
-                                         prelayouts:(void (^)(UIView *fromView, UIView *toView))preparation
-                                         animations:(void (^)(UIView *fromView, UIView *toView))animations
-                                         completion:(void (^)(UIView *fromView, UIView *toView))completion;
+                                         prelayouts:(YAAnimationLayoutViewsBlock)preparation
+                                         animations:(YAAnimationLayoutViewsBlock)animations
+                                         completion:(YAAnimationLayoutViewsBlock)completion;
+
+- (UIViewController *)popViewControllerWithDuration:(NSTimeInterval)duration
+                               controllerPrelayouts:(YAAnimationLayoutViewControllersBlock)preparation
+                               controllerAnimations:(YAAnimationLayoutViewControllersBlock)animations
+                                         completion:(YAAnimationLayoutViewControllersBlock)completion;
+
+- (UIViewController *)popViewControllerWithDuration:(NSTimeInterval)duration
+                                            options:(UIViewAnimationOptions)options
+                               controllerPrelayouts:(YAAnimationLayoutViewControllersBlock)preparation
+                               controllerAnimations:(YAAnimationLayoutViewControllersBlock)animations
+                                         completion:(YAAnimationLayoutViewControllersBlock)completion;
+
+- (NSArray *)popToViewController:(UIViewController *)viewController
+                        duration:(NSTimeInterval)duration
+                      prelayouts:(YAAnimationLayoutViewsBlock)preparation
+                      animations:(YAAnimationLayoutViewsBlock)animations
+                      completion:(YAAnimationLayoutViewsBlock)completion;
+
+- (NSArray *)popToViewController:(UIViewController *)viewController
+                        duration:(NSTimeInterval)duration
+            controllerPrelayouts:(YAAnimationLayoutViewControllersBlock)preparation
+            controllerAnimations:(YAAnimationLayoutViewControllersBlock)animations
+                      completion:(YAAnimationLayoutViewControllersBlock)completion;
 
 - (NSArray *)popToViewController:(UIViewController *)viewController
                         duration:(NSTimeInterval)duration
                          options:(UIViewAnimationOptions)options
-                      prelayouts:(void (^)(UIView *fromView, UIView *toView))preparation
-                      animations:(void (^)(UIView *fromView, UIView *toView))animations
-                      completion:(void (^)(UIView *fromView, UIView *toView))completion;
+                      prelayouts:(YAAnimationLayoutViewsBlock)preparation
+                      animations:(YAAnimationLayoutViewsBlock)animations
+                      completion:(YAAnimationLayoutViewsBlock)completion;
+
+- (NSArray *)popToViewController:(UIViewController *)viewController
+                        duration:(NSTimeInterval)duration
+                         options:(UIViewAnimationOptions)options
+            controllerPrelayouts:(YAAnimationLayoutViewControllersBlock)preparation
+            controllerAnimations:(YAAnimationLayoutViewControllersBlock)animations
+                      completion:(YAAnimationLayoutViewControllersBlock)completion;
 
 @end
--- YAUIKit/YAUIKit/Source/Extend/UINavigationController/UINavigationController+YAAnimation.m
+++ YAUIKit/YAUIKit/Source/Extend/UINavigationController/UINavigationController+YAAnimation.m
@@ -10,6 +10,8 @@
 
 @implementation UINavigationController (YAAnimation)
 
+#pragma mark - Push
+
 - (void)pushViewController:(UIViewController *)toViewController
                   duration:(NSTimeInterval)duration
                 prelayouts:(void (^)(UIView *, UIView *))preparation
@@ -18,7 +20,7 @@
 {
   [self pushViewController:toViewController
                   duration:duration
-                   options:0
+                   options:UIViewAnimationOptionCurveEaseIn
                 prelayouts:preparation
                 animations:animations
                 completion:completion];
@@ -31,42 +33,84 @@
                 animations:(void (^)(UIView *, UIView *))animations
                 completion:(void (^)(UIView *, UIView *))completion
 {
+  [self pushViewController:toViewController
+                  duration:duration
+                   options:options
+      controllerPrelayouts:^(UIViewController *fromViewController, UIViewController *toViewController)
+  {
+    if (preparation) {
+      preparation(fromViewController.view, toViewController.view);
+    }
+  } controllerAnimations:^(UIViewController *fromViewController, UIViewController *toViewController) {
+    if (animations) {
+      animations(fromViewController.view, toViewController.view);
+    }
+  } completion:^(UIViewController *fromViewController, UIViewController *toViewController) {
+    if (completion) {
+      completion(fromViewController.view, toViewController.view);
+    }
+  }];
+}
+
+- (void)pushViewController:(UIViewController *)toViewController
+                  duration:(NSTimeInterval)duration
+      controllerPrelayouts:(void (^)(UIViewController *fromViewController, UIViewController *toViewController))preparation
+      controllerAnimations:(void (^)(UIViewController *fromViewController, UIViewController *toViewController))animations
+                completion:(void (^)(UIViewController *fromViewController, UIViewController *toViewController))completion
+{
+  [self pushViewController:toViewController
+                  duration:duration
+                   options:UIViewAnimationOptionCurveEaseIn 
+      controllerPrelayouts:preparation
+      controllerAnimations:animations
+                completion:completion];
+}
+
+- (void)pushViewController:(UIViewController *)toViewController
+                  duration:(NSTimeInterval)duration
+                   options:(UIViewAnimationOptions)options
+      controllerPrelayouts:(void (^)(UIViewController *fromViewController, UIViewController *toViewController))preparation
+      controllerAnimations:(void (^)(UIViewController *fromViewController, UIViewController *toViewController))animations
+                completion:(void (^)(UIViewController *fromViewController, UIViewController *toViewController))completion
+{
   __block UIViewController *fromViewController = self.visibleViewController;
-  
+
   [self pushViewController:toViewController animated:NO];
   [self.view layoutIfNeeded] ;
-  
+
   __block UIView *superView = toViewController.view.superview;
   [superView insertSubview:fromViewController.view
               belowSubview:toViewController.view];
-  
+
   if (preparation) {
-    preparation(fromViewController.view, toViewController.view);
+    preparation(fromViewController, toViewController);
   }
-  
+
   [UIView animateWithDuration:duration
                         delay:0.0
                       options:options
                    animations:^{
                      if (animations) {
-                       animations(fromViewController.view, toViewController.view);
+                       animations(fromViewController, toViewController);
                      }
                    }
                    completion:^(BOOL finished) {
                      if (completion) {
-                       completion(fromViewController.view, toViewController.view);
+                       completion(fromViewController, toViewController);
                      }
                      [fromViewController.view removeFromSuperview];
                    }];
 }
 
+#pragma mark - Pop
+
 - (UIViewController *)popViewControllerWithDuration:(NSTimeInterval)duration
-                                         prelayouts:(void (^)(UIView *, UIView *))preparation
-                                         animations:(void (^)(UIView *, UIView *))animations
-                                         completion:(void (^)(UIView *, UIView *))completion
+                                         prelayouts:(YAAnimationLayoutViewsBlock)preparation
+                                         animations:(YAAnimationLayoutViewsBlock)animations
+                                         completion:(YAAnimationLayoutViewsBlock)completion
 {
   UIViewController *fromViewController = [self popViewControllerWithDuration:duration
-                                                                     options:0
+                                                                     options:UIViewAnimationOptionCurveEaseIn
                                                                   prelayouts:preparation
                                                                   animations:animations
                                                                   completion:completion];
@@ -75,9 +119,9 @@
 
 - (UIViewController *)popViewControllerWithDuration:(NSTimeInterval)duration
                                             options:(UIViewAnimationOptions)options
-                                         prelayouts:(void (^)(UIView *, UIView *))preparation
-                                         animations:(void (^)(UIView *, UIView *))animations
-                                         completion:(void (^)(UIView *, UIView *))completion
+                                         prelayouts:(YAAnimationLayoutViewsBlock)preparation
+                                         animations:(YAAnimationLayoutViewsBlock)animations
+                                         completion:(YAAnimationLayoutViewsBlock)completion
 {
   NSInteger count = [self.viewControllers count];
   if (count <= 1) {
@@ -92,12 +136,97 @@
                          completion:completion] lastObject];
 }
 
+- (UIViewController *)popViewControllerWithDuration:(NSTimeInterval)duration
+                               controllerPrelayouts:(YAAnimationLayoutViewControllersBlock)preparation
+                               controllerAnimations:(YAAnimationLayoutViewControllersBlock)animations
+                                         completion:(YAAnimationLayoutViewControllersBlock)completion
+{
+  return [self popViewControllerWithDuration:duration
+                                     options:UIViewAnimationOptionCurveEaseIn
+                        controllerPrelayouts:preparation
+                        controllerAnimations:animations
+                                  completion:completion];
+}
+
+- (UIViewController *)popViewControllerWithDuration:(NSTimeInterval)duration
+                                            options:(UIViewAnimationOptions)options
+                               controllerPrelayouts:(YAAnimationLayoutViewControllersBlock)preparation
+                               controllerAnimations:(YAAnimationLayoutViewControllersBlock)animations
+                                         completion:(YAAnimationLayoutViewControllersBlock)completion
+{
+  NSInteger count = [self.viewControllers count];
+  if (count <= 1) {
+    return nil;
+  }
+
+  return [[self popToViewController:[[self viewControllers] objectAtIndex:(count - 2)]
+                           duration:duration
+                            options:options
+               controllerPrelayouts:preparation
+               controllerAnimations:animations
+                         completion:completion] lastObject];
+}
+
+- (NSArray *)popToViewController:(UIViewController *)viewController
+                        duration:(NSTimeInterval)duration
+                      prelayouts:(YAAnimationLayoutViewsBlock)preparation
+                      animations:(YAAnimationLayoutViewsBlock)animations
+                      completion:(YAAnimationLayoutViewsBlock)completion
+{
+  return [self popToViewController:viewController
+                          duration:duration
+                           options:UIViewAnimationOptionCurveEaseIn
+                        prelayouts:preparation
+                        animations:animations
+                        completion:completion];
+}
+
 - (NSArray *)popToViewController:(UIViewController *)viewController
                         duration:(NSTimeInterval)duration
                          options:(UIViewAnimationOptions)options
-                      prelayouts:(void (^)(UIView *fromView, UIView *toView))preparation
-                      animations:(void (^)(UIView *fromView, UIView *toView))animations
-                      completion:(void (^)(UIView *fromView, UIView *toView))completion
+                      prelayouts:(YAAnimationLayoutViewsBlock)preparation
+                      animations:(YAAnimationLayoutViewsBlock)animations
+                      completion:(YAAnimationLayoutViewsBlock)completion
+{
+  return [self popToViewController:viewController
+                          duration:duration
+                           options:options
+              controllerPrelayouts:^(UIViewController *fromViewController, UIViewController *toViewController)
+  {
+    if (preparation) {
+      preparation(fromViewController.view, toViewController.view);
+    }
+  } controllerAnimations:^(UIViewController *fromViewController, UIViewController *toViewController) {
+    if (animations) {
+      animations(fromViewController.view, toViewController.view);
+    }
+  } completion:^(UIViewController *fromViewController, UIViewController *toViewController) {
+    if (completion) {
+      completion(fromViewController.view, toViewController.view);
+    }
+  }];
+}
+
+- (NSArray *)popToViewController:(UIViewController *)viewController
+                        duration:(NSTimeInterval)duration
+            controllerPrelayouts:(YAAnimationLayoutViewControllersBlock)preparation
+            controllerAnimations:(YAAnimationLayoutViewControllersBlock)animations
+                      completion:(YAAnimationLayoutViewControllersBlock)completion
+{
+  return [self popToViewController:viewController
+                          duration:duration
+                           options:UIViewAnimationOptionCurveEaseIn
+              controllerPrelayouts:preparation
+              controllerAnimations:animations
+                        completion:completion];
+}
+
+- (NSArray *)popToViewController:(UIViewController *)viewController
+                        duration:(NSTimeInterval)duration
+                         options:(UIViewAnimationOptions)options
+            controllerPrelayouts:(YAAnimationLayoutViewControllersBlock)preparation
+            controllerAnimations:(YAAnimationLayoutViewControllersBlock)animations
+                      completion:(YAAnimationLayoutViewControllersBlock)completion
 {
   if ([self.viewControllers indexOfObject:viewController] == NSNotFound) {
     return nil;
@@ -113,7 +242,7 @@
 
   [self.view layoutIfNeeded];
   if (preparation) {
-    preparation(fromViewController.view, toViewController.view);
+    preparation(fromViewController, toViewController);
   }
 
   [UIView animateWithDuration:duration
@@ -121,12 +250,12 @@
                       options:options
                    animations:^{
                      if (animations) {
-                       animations(fromViewController.view, toViewController.view);
+                       animations(fromViewController, toViewController);
                      }
                    }
                    completion:^(BOOL finished) {
                      if (completion) {
-                       completion(fromViewController.view, toViewController.view);
+                       completion(fromViewController, toViewController);
                      }
                      [fromViewController.view removeFromSuperview];
                    }];
