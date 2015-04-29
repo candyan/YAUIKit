06e2b8a8b51bc39e5a29239833cef2e6d2c1dadc
Candyan <liuyanhp@gmail.com>
2015-04-29 11:05:52 +0800

    add 2.5.2

--- Source/YARefreshControl/YARefreshControl.m
+++ Source/YARefreshControl/YARefreshControl.m
@@ -30,16 +30,16 @@
         // set ivars
         _scrollView = scrollView;
         _originContentInsets = scrollView.contentInset;
-        
+
         _canRefreshDirection = kYARefreshableDirectionNone;
-        
+
         // observe the contentOffset. NSKeyValueObservingOptionPrior is CRUCIAL!
         [_scrollView
-         addObserver:self
-         forKeyPath:@"contentOffset"
-         options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld | NSKeyValueObservingOptionPrior
-         context:NULL];
-        
+            addObserver:self
+             forKeyPath:@"contentOffset"
+                options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld | NSKeyValueObservingOptionPrior
+                context:NULL];
+
         [self setRefreshView:[self _defaultRefreshViewForDirection:kYARefreshDirectionBottom]
                 forDirection:kYARefreshDirectionBottom];
         [self setRefreshView:[self _defaultRefreshViewForDirection:kYARefreshDirectionTop]
@@ -79,20 +79,20 @@
         [refreshView removeFromSuperview];
     }
     [self.refreshViews setObject:customView forKey:@(direction)];
-    
+
     if (direction == kYARefreshDirectionTop && [UIDevice currentDevice].systemVersion.floatValue >= 7.0f) {
         [self.scrollView insertSubview:customView atIndex:0];
     } else {
         [self.scrollView addSubview:customView];
     }
-    
+
     [self _layoutRefreshViewForDirection:direction];
 }
 
 - (void)setCanRefreshDirection:(YARefreshableDirection)canRefreshDirection
 {
     _canRefreshDirection = canRefreshDirection;
-    
+
     for (NSInteger index = 0; index < 4; index++) {
         YARefreshDirection direction = 1 << index;
         UIView *refreshView = [self refreshViewAtDirection:direction];
@@ -111,7 +111,7 @@
         YARefreshView *refreshView = (YARefreshView *)[self refreshViewAtDirection:direction];
         [refreshView layoutSubviewsForRefreshState:refreshState];
     }
-    
+
     if ([self.delegate respondsToSelector:@selector(refreshControl:didRefreshStateChanged:atDirection:)]) {
         [self.delegate refreshControl:self didRefreshStateChanged:refreshState atDirection:direction];
     }
@@ -149,18 +149,18 @@
     if (self.refreshingDirection == direction) {
         return;
     }
-    
+
     YARefreshableDirection refreshableDirection = kYARefreshableDirectionNone;
     UIEdgeInsets contentInset = _scrollView.contentInset;
     CGPoint contentOffset = CGPointZero;
-    
+
     UIView *refreshView = [self refreshViewAtDirection:direction];
-    
+
     CGFloat refreshingInset = CGRectGetHeight(refreshView.frame);
     if ([self.delegate respondsToSelector:@selector(refreshControl:refreshingInsetForDirection:)]) {
         refreshingInset = [self.delegate refreshControl:self refreshingInsetForDirection:direction];
     }
-    
+
     switch (direction) {
         case kYARefreshDirectionTop:
             refreshableDirection = kYARefreshableDirectionTop;
@@ -177,7 +177,7 @@
         case kYARefreshDirectionBottom:
             refreshableDirection = kYARefreshableDirectionBottom;
             contentOffset =
-            CGPointMake(0, _scrollView.contentSize.height - _scrollView.bounds.size.height + contentInset.bottom);
+                CGPointMake(0, _scrollView.contentSize.height - _scrollView.bounds.size.height + contentInset.bottom);
             break;
         case kYARefreshDirectionRight:
             refreshableDirection = kYARefreshableDirectionRight;
@@ -188,23 +188,23 @@
         default:
             break;
     }
-    
+
     [self _setRefreshState:kYARefreshStateLoading atDirection:direction];
-    
+
     self.refreshingDirection |= direction;
     self.refreshableDirection &= ~refreshableDirection;
-    
+
     NSTimeInterval duration = flag ? 0.3f : 0.0f;
     [UIView animateWithDuration:duration
-                     animations:^{
-                         _scrollView.contentInset = contentInset;
-                         _scrollView.contentOffset = contentOffset;
-                     }
-                     completion:^(BOOL finished) {
-                         if (self.refreshHandleAction) {
-                             self.refreshHandleAction(direction);
-                         }
-                     }];
+        animations:^{
+          _scrollView.contentInset = contentInset;
+          _scrollView.contentOffset = contentOffset;
+        }
+        completion:^(BOOL finished) {
+          if (self.refreshHandleAction) {
+              self.refreshHandleAction(direction);
+          }
+        }];
 }
 
 - (void)stopRefreshAtDirection:(YARefreshDirection)direction completion:(void (^)())completion
@@ -216,50 +216,50 @@
 {
     [self _setRefreshState:kYARefreshStateStop atDirection:direction];
     NSTimeInterval duration = flag ? 0.4f : 0.0f;
-    
+
     [UIView animateWithDuration:duration
-                     animations:^{
-                         _scrollView.contentInset = self.originContentInsets;
-                     }
-                     completion:^(BOOL finished) {
-                         if (finished) {
-                             self.refreshingDirection &= ~direction;
-                             if (completion) {
-                                 completion();
-                             }
-                         }
-                     }];
+        animations:^{
+          _scrollView.contentInset = self.originContentInsets;
+        }
+        completion:^(BOOL finished) {
+          if (finished) {
+              self.refreshingDirection &= ~direction;
+              if (completion) {
+                  completion();
+              }
+          }
+        }];
 }
 
 #pragma mark - Private Methods
 
 - (void)_checkOffsetsForDirection:(YARefreshDirection)direction change:(NSDictionary *)change
 {
-    
+
     // define some local ivars that disambiguates according to direction
     CGPoint oldOffset = [[change objectForKey:NSKeyValueChangeOldKey] CGPointValue];
     if (direction == kYARefreshDirectionTop || direction == kYARefreshDirectionBottom) {
         [self _layoutRefreshViewForDirection:direction];
     }
-    
+
     UIView *refreshView = [self refreshViewAtDirection:direction];
-    
+
     YARefreshDirection refreshingDirection = direction;
     YARefreshableDirection refreshableDirection = kYARefreshableDirectionNone;
     BOOL canEngage = NO;
     UIEdgeInsets contentInset = _scrollView.contentInset;
-    
+
     CGFloat refreshViewHeight = refreshView.frame.size.height;
     CGFloat refreshableInset = refreshViewHeight;
     if ([self.delegate respondsToSelector:@selector(refreshControl:refreshableInsetForDirection:)]) {
         refreshableInset = [self.delegate refreshControl:self refreshableInsetForDirection:direction];
     }
-    
+
     CGFloat refreshingInset = refreshViewHeight;
     if ([self.delegate respondsToSelector:@selector(refreshControl:refreshingInsetForDirection:)]) {
         refreshingInset = [self.delegate refreshControl:self refreshingInsetForDirection:direction];
     }
-    
+
     CGFloat threshold = 0.0f;
     switch (direction) {
         case kYARefreshDirectionTop:
@@ -296,16 +296,16 @@
         default:
             break;
     }
-    
+
     canEngage = (threshold > refreshableInset);
-    
+
     if ([self.delegate respondsToSelector:@selector(refreshControl:didShowRefreshViewHeight:atDirection:)] &&
         !canEngage) {
         threshold = MAX(threshold, 0);
         threshold = MIN(threshold, CGRectGetHeight(refreshView.bounds));
         [self.delegate refreshControl:self didShowRefreshViewHeight:threshold atDirection:direction];
     }
-    
+
     if (!(self.refreshingDirection & refreshingDirection)) {
         // only go in here if the requested direction is enabled and not refreshing
         if (canEngage) {
@@ -318,13 +318,13 @@
                 self.refreshableDirection &= ~refreshableDirection;
                 [self _setRefreshState:kYARefreshStateLoading atDirection:direction];
                 [UIView animateWithDuration:.2f
-                                 animations:^{
-                                     _scrollView.contentInset = contentInset;
-                                 }
-                                 completion:^(BOOL finished) {
-                                     if (self.refreshHandleAction)
-                                         self.refreshHandleAction(direction);
-                                 }];
+                    animations:^{
+                      _scrollView.contentInset = contentInset;
+                    }
+                    completion:^(BOOL finished) {
+                      if (self.refreshHandleAction)
+                          self.refreshHandleAction(direction);
+                    }];
             } else if (_scrollView.dragging && !_scrollView.decelerating &&
                        !(self.refreshableDirection & refreshableDirection)) {
                 // only go in here the first time you've dragged past releasable offset
@@ -345,21 +345,21 @@
 {
     UIView *refreshView = [self refreshViewAtDirection:direction];
     CGFloat originY = 0.0f;
-    
+
     if (_canRefreshDirection & direction) {
         [refreshView setHidden:NO];
         switch (direction) {
             case kYARefreshDirectionTop:
                 originY = self.scrollView.contentOffset.y + self.originContentInsets.top + self.refreshViewInsets.top;
                 break;
-                
+
             case kYARefreshDirectionBottom:
                 originY = ((self.scrollView.contentSize.height > self.scrollView.frame.size.height)
-                           ? self.scrollView.contentSize.height + self.originContentInsets.bottom
-                           : self.scrollView.frame.size.height);
+                               ? self.scrollView.contentSize.height + self.originContentInsets.bottom
+                               : self.scrollView.frame.size.height);
                 originY += self.originContentInsets.bottom;
                 break;
-                
+
             default:
                 break;
         }
